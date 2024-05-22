43d6c422c80385d8df7a4cd50f950d5b7db8535d
DenisKhudyakov <denis.hudyakov27@gmail.com>
2024-05-22 22:36:03 +0500

    создал модели отчета, но скорее всего нужно доработать....

--- station_report/models.py
+++ station_report/models.py
@@ -1,3 +1,32 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Station(models.Model):
+    """Модель станции"""
+    station_number = models.CharField(max_length=50, unique=True, verbose_name='station')
+
+    def __str__(self):
+        return self.station_number
+
+
+class Section(models.Model):
+    """Модель этапа производства"""
+    station = models.ForeignKey(Station, on_delete=models.CASCADE)
+    section_name = models.CharField(max_length=50)
+    is_completed = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.station} - {self.section_name}"
+
+
+class SectionDate(models.Model):
+    """Модель дата выдачи СЗ и дата выдачи РД"""
+    section = models.ForeignKey(Section, on_delete=models.CASCADE)
+    issue_date_SZ = models.DateField(null=True, blank=True)
+    issue_date_RD = models.DateField(null=True, blank=True)
+    comment = models.TextField(blank=True)
+
+    def __str__(self):
+        return f"{self.section} - Dates"
