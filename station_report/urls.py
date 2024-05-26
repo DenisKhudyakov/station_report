edd3187bdb7a378b6f892e8034453565e7ceefed
DenisKhudyakov <denis.hudyakov27@gmail.com>
2024-05-26 21:14:39 +0500

    написан тестовый html и написан html отчет

--- station_report/urls.py
+++ station_report/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
 from station_report.apps import StationReportConfig
-from station_report.views import home
+from station_report.views import home, StationReportView
 
 app_name = StationReportConfig.name
 
 urlpatterns = [
     path('', home, name='home'),
+    path('report_view/', StationReportView.as_view(), name='report_view'),
 ]
--- station_report/views.py
+++ station_report/views.py
@@ -9,16 +9,16 @@ def home(request):
 
 
 class StationReportView(ListView):
-    template_name ='station_report/report.html'
+    template_name = 'station_report/report.html'
     model = Station
 
     def get_context_data(self, *, object_list=None, **kwargs):
         context = super().get_context_data(**kwargs)
-        station_id = self.kwargs.get('pk')
-        station = get_object_or_404(Station, id=station_id)
-        sections = Section.objects.filter(station=station)
-        section_data = SectionDate.objects.filter(section__in=sections)
-        context['station'] = station
-        context['sections'] = sections
-        context['section_data'] = section_data
+        stations = Station.objects.all()
+        for station in stations:
+            sections = Section.objects.filter(station=station)
+            section_data = SectionDate.objects.filter(section__in=sections)
+            context['stations'] = stations
+            context['sections'] = sections
+            context['section_data'] = section_data
         return context
