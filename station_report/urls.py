660dd826b9c2b2b7b787113569a21fcea13c24b1
DenisKhudyakov <denis.hudyakov27@gmail.com>
2024-07-15 21:01:45 +0500

    create new html

--- station_report/urls.py
+++ station_report/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 
 from station_report.apps import StationReportConfig
-from station_report.views import home, StationReportView
+from station_report.views import home, StationReportView, TestView
 
 app_name = StationReportConfig.name
 
 urlpatterns = [
     path("", home, name="home"),
     path("report_view/", StationReportView.as_view(), name="report_view"),
+    path("test", TestView.as_view(), name="test"),
 ]
