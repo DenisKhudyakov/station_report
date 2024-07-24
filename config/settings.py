f7004f4503ac14425e86b3839108927154042242
DenisKhudyakov <denis.hudyakov27@gmail.com>
2024-07-24 20:56:48 +0500

    update html

--- config/settings.py
+++ config/settings.py
@@ -9,11 +9,18 @@ https://docs.djangoproject.com/en/5.0/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.0/ref/settings/
 """
-
+import os
+from dotenv import load_dotenv, find_dotenv
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+if not find_dotenv():
+    exit('Error loading.env file')
+else:
+    load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
@@ -25,7 +32,7 @@ SECRET_KEY = "django-insecure-*wv*a*^ybtnu$%gx@-t@ycb-pk13^a59klsgy!csows47=ip-b
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS").split(",")
 
 
 # Application definition
@@ -81,10 +88,11 @@ WSGI_APPLICATION = "config.wsgi.application"
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "station_report",
-        "USER": "postgres",
-        "PASSWORD": "279211",
-        "PORT": 5432,
+        "NAME": os.getenv('DB_NAME'),
+        "USER": os.getenv('DB_USER'),
+        "PASSWORD": os.getenv('DB_PASSWORD'),
+        "PORT": os.getenv('DB_PORT'),
+        "HOST": os.getenv('DB_HOST'),
 
     }
 }
